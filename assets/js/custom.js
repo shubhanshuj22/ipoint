7323451f1278419c6ec4f27d116ec2355feaa305
Shubhanshu Jain <51177359+shubhanshuj22@users.noreply.github.com>
2021-07-23 21:50:06 +0530

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -244,3 +244,30 @@
 
 
 })(window.jQuery);
+
+{
+	document.addEventListener('contextmenu', function(e) {
+		e.preventDefault();
+	  });
+	  document.onkeydown = function(e) {
+		if(event.keyCode == 123) {
+		   return false;
+		}
+		if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+		   return false;
+		}
+		if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+		   return false;
+		}
+		if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+		   return false;
+		}
+		if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+		   return false;
+		}
+	  }
+	}
+	
+	$('.carousel').carousel({
+		interval: 5000
+	  })
